e9fc85f6584a5801031eed26eeb292e3e88b3567
이종혁 <whdgur57117@gmail.com>
2024-07-03 03:32:24 +0900

    [Test] tsconfigPaths로 변경

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import tsconfigPaths from "vite-tsconfig-paths"
-import * as path from "path"
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@styled": path.resolve(__dirname, "styled-system"),
-    },
-  },
+  plugins: [react(), tsconfigPaths()],
+
   build: {
     rollupOptions: {
       external: [/@styled/],
