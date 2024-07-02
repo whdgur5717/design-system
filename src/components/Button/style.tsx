a0e259c5a42740059d9291455221b99e519e33a5
이종혁 <whdgur57117@gmail.com>
2024-07-03 00:52:45 +0900

    [Test] 경로에 index.ts 추가

--- src/components/Button/style.tsx
+++ src/components/Button/style.tsx
@@ -1,5 +1,5 @@
-import { cva, type RecipeVariantProps } from "@/styled-system/css"
-import { styled } from "@/styled-system/jsx"
+import { cva, type RecipeVariantProps } from "@/styled-system/css/index.ts"
+import { styled } from "@/styled-system/jsx/index.ts"
 
 export const buttonCva = cva({
   base: {
