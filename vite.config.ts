85a78b6b10b1058b25abc3144ad4c5593e0fe48a
이종혁 <whdgur57117@gmail.com>
2024-07-03 00:54:57 +0900

    [Test] tsconfigPaths 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
-import * as path from "path"
+import tsconfigPaths from "vite-tsconfig-paths"
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
-  },
+  plugins: [react(), tsconfigPaths()],
 })
