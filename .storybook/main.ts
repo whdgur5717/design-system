80e07d3590b2ea5d1ef507cb7d65d5532409985a
이종혁 <whdgur57117@gmail.com>
2024-07-05 21:54:57 +0900

    @storybook-addon-ally 설치 및 설정 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -9,6 +9,7 @@ const config: StorybookConfig = {
     "@storybook/addon-interactions",
     "@storybook/addon-controls",
     "@storybook/addon-docs",
+    "@storybook/addon-a11y",
   ],
   framework: {
     name: "@storybook/react-vite",
