e7ce8d79d95f40af1ba7ff7f278221300e6f5bf0
이종혁 <whdgur57117@gmail.com>
2024-07-22 22:11:26 +0900

    아코디언 content 높이 계산 hook 생성

---
+++ packages/ui/src/components/Accordion/useAccordionHeight.ts
@@ -0,0 +1,34 @@
+import { useEffect, useRef } from "react"
+
+export const useAccordionHeight = <T extends HTMLElement>(isOpen: boolean) => {
+  const contentRef = useRef<T>(null)
+  useEffect(() => {
+    const element = contentRef.current
+    if (element === null || !element.parentElement) {
+      return
+    }
+    const { parentElement } = element
+    if (isOpen) {
+      parentElement.style.display = "block"
+
+      const height = element.style.getPropertyValue("--accordion-height")
+
+      if (height === "0" || !height) {
+        element.parentElement!.style.setProperty(
+          "--accordion-height",
+          `${element.clientHeight}px`,
+        )
+      }
+    } else {
+      setTimeout(() => {
+        parentElement.style.display = "none"
+        parentElement.style.setProperty(
+          "--accordion-height",
+          `${element.clientHeight}px`,
+        )
+      }, 150)
+    }
+  }, [isOpen])
+
+  return contentRef
+}
