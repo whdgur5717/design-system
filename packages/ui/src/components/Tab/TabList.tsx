574ad071da7fdc904e1da1503620db04c8d40245
이종혁 <whdgur57117@gmail.com>
2024-07-22 22:59:02 +0900

    tabList에 키보드이벤트 추가 및 aria속성 추가

--- packages/ui/src/components/Tab/TabList.tsx
+++ packages/ui/src/components/Tab/TabList.tsx
@@ -1,15 +1,28 @@
 import type { ReactNode } from "react"
 import { useTabContext } from "./Tab"
 import Slot from "../Slot/Slot"
-import { cx } from "jh-generated/cx"
-import { css } from "jh-generated/css"
+import useKeyboardEvent from "../../hooks/useKeyboardEvent"
 
 interface TabListProps {
   children: ReactNode
 }
 export const TabList = ({ children }: TabListProps) => {
-  const { selected, onSelect } = useTabContext("tab")
-  return <div role="tablist">{children}</div>
+  const { refs, handleKeyDown } = useKeyboardEvent({
+    keyList: ["Home", "End", "ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"],
+  })
+
+  return (
+    <div
+      role="tablist"
+      tabIndex={0}
+      ref={(node) => {
+        refs.current = Array.from(node?.children || []) as HTMLElement[]
+      }}
+      onKeyDown={handleKeyDown}
+    >
+      {children}
+    </div>
+  )
 }
 //클릭된 아이템을 제외하고는 selected 제거해주기
 
@@ -32,10 +45,9 @@ export const TabItem = ({
 
   return (
     <Comp
-      className={cx(
-        css({ color: isSelected ? "blue_100" : "red_200" }),
-        className,
-      )}
+      role="tab"
+      tabIndex={0}
+      aria-selected={isSelected}
       onClick={() => onSelect?.(value)}
     >
       {children}
