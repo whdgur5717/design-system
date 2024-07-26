35309a8a9766abd2963bc8017114e259ffbeb8d2
이종혁 <whdgur57117@gmail.com>
2024-07-26 18:36:36 +0900

    preset accordion 추가

--- packages/ui/src/components/preset.ts
+++ packages/ui/src/components/preset.ts
@@ -1,12 +1,14 @@
 import { definePreset } from "@pandacss/dev"
 import { buttonRecipe } from "./Button/style"
 import { tagButtonRecipe } from "./TagButton/style"
+import { accordionRecipe } from "./Accordion/style"
 
 export const preset = definePreset({
   theme: {
     recipes: {
       button: buttonRecipe,
       tagButton: tagButtonRecipe,
+      accordion: accordionRecipe,
     },
   },
 })
