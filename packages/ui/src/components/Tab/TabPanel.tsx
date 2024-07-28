9d48079dc83abfd31c2bd25a2f1b02041bcdc977
이종혁 <whdgur57117@gmail.com>
2024-07-28 19:32:00 +0900

    TabPanel , TabContent 컴포넌트 기본 로직 추가

--- packages/ui/src/components/Tab/TabPanel.tsx
+++ packages/ui/src/components/Tab/TabPanel.tsx
@@ -1,7 +1,31 @@
+import type { ReactNode } from "react"
 import { useTabContext } from "./Tab"
+import { css } from "jh-generated/css"
 
-interface TabPanelProps {}
-export const TabPanel = (TabPanelProps) => {
+interface TabPanelProps {
+  children: ReactNode
+  className?: string
+}
+export const TabPanel = ({ children }: TabPanelProps) => {
+  return <div tabIndex={0}>{children}</div>
+}
+interface TabContentProps {
+  children: ReactNode
+  value: string
+}
+export const TabContent = ({ children, value }: TabContentProps) => {
   const { selected } = useTabContext("tab")
-  return <div></div>
+
+  const isSelected = selected === value
+
+  return (
+    <div
+      tabIndex={0}
+      className={css({
+        display: isSelected ? "block" : "none",
+      })}
+    >
+      {children}
+    </div>
+  )
 }
