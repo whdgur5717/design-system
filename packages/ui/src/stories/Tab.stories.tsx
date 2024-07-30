70d2c2007edae99b67d31da3568eff30696aa1b2
이종혁 <whdgur57117@gmail.com>
2024-07-30 16:21:55 +0900

    Tab Controlled story 추가

--- packages/ui/src/stories/Tab.stories.tsx
+++ packages/ui/src/stories/Tab.stories.tsx
@@ -1,5 +1,5 @@
-import { css } from "jh-generated/css"
-import { Tab, TabList, TabItem } from "../components/Tab/index"
+import { useState } from "react"
+import { Tab, TabList, TabItem, TabContent } from "../components/Tab/index"
 import type { Meta, StoryObj } from "@storybook/react"
 export default {
   title: "Tab",
@@ -19,7 +19,39 @@ export const Primary: Story = {
           </TabItem>
           <TabItem value="2">2</TabItem>
         </TabList>
+        <TabContent value="1">
+          <div>1의 내용</div>
+        </TabContent>
+        <TabContent value="2">
+          <div>2의 내용</div>
+        </TabContent>
       </>
     ),
+  },
+}
+
+export const Controlled: Story = {
+  render: () => {
+    const [selectedItems, setSelectedItems] = useState<string>("2")
+
+    const handleValueChange = (newSelectedItems: string) => {
+      setSelectedItems(newSelectedItems)
+    }
+    return (
+      <Tab selected={selectedItems} onSelect={handleValueChange}>
+        <TabList>
+          <TabItem value="1" asChild>
+            <div>1</div>
+          </TabItem>
+          <TabItem value="2">2</TabItem>
+        </TabList>
+        <TabContent value="1">
+          <div>1의 내용</div>
+        </TabContent>
+        <TabContent value="2">
+          <div>2의 내용</div>
+        </TabContent>
+      </Tab>
+    )
   },
 }
