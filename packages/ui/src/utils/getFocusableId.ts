24a9099e02a69b37a580852295b266fc317c395f
이종혁 <whdgur57117@gmail.com>
2024-08-04 19:57:54 +0900

    키보드 포커스이동 util함수 생성

---
+++ packages/ui/src/utils/getFocusableId.ts
@@ -0,0 +1,31 @@
+import type { RovingTabIndexItem } from "../components/Tab/useRovingTabIndex"
+
+export function getNextFocusableId(
+  orderedItems: RovingTabIndexItem[],
+  value: string,
+): RovingTabIndexItem | undefined {
+  const currIndex = orderedItems.findIndex((item) => item.value === value)
+  return orderedItems.at(
+    currIndex === orderedItems.length - 1 ? 0 : currIndex + 1,
+  )
+}
+
+export function getPrevFocusableId(
+  orderedItems: RovingTabIndexItem[],
+  value: string,
+): RovingTabIndexItem | undefined {
+  const currIndex = orderedItems.findIndex((item) => item.value === value)
+  return orderedItems.at(currIndex === 0 ? -1 : currIndex - 1)
+}
+
+export function getFirstFocusableId(
+  orderedItems: RovingTabIndexItem[],
+): RovingTabIndexItem | undefined {
+  return orderedItems.at(0)
+}
+
+export function getLastFocusableId(
+  orderedItems: RovingTabIndexItem[],
+): RovingTabIndexItem | undefined {
+  return orderedItems.at(-1)
+}
