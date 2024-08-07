85fdf0ab4add47888e2748c385b41d330ab2147d
이종혁 <whdgur57117@gmail.com>
2024-08-07 23:20:28 +0900

    Slider 스타일 적용

--- packages/ui/src/components/Slider/style.ts
+++ packages/ui/src/components/Slider/style.ts
@@ -2,10 +2,37 @@ import { define } from "../../../dev"
 
 export const sliderRecipe = define.recipe({
   className: "slider",
+  base: {
+    WebkitAppearance: "none",
+    height: "4px",
+    borderRadius: "lg",
+    backgroundColor: "grey_300",
+    backgroundImage: "linear-gradient(#7DD3FC, #7DD3FC)",
+    backgroundSize: "var(--range-size)",
+    backgroundRepeat: "no-repeat",
+    "&::-webkit-slider-thumb": {
+      WebkitAppearance: "none",
+      width: "20px",
+      height: "20px",
+      backgroundColor: "grey_300",
+      borderRadius: "rounded",
+      border: "none",
+      cursor: "pointer",
+    },
+    "&::-moz-range-thumb": {
+      WebkitAppearance: "none",
+      width: "20px",
+      height: "20px",
+      backgroundColor: "grey_300",
+      borderRadius: "rounded",
+      border: "none",
+      cursor: "pointer",
+    },
+  },
   variants: {
     orientation: {
       horizontal: {
-        writingMode: "vertical-rl",
+        writingMode: "horizontal-tb",
       },
       vertical: {
         writingMode: "vertical-rl",
