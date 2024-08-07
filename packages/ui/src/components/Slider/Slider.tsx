28613a10b9ff721d6e297168388883ac8a143e43
이종혁 <whdgur57117@gmail.com>
2024-08-07 23:21:36 +0900

    value 변경에 따라 background 색 변경하는 로직 추가 및 props 선언방식 변경

--- packages/ui/src/components/Slider/Slider.tsx
+++ packages/ui/src/components/Slider/Slider.tsx
@@ -1,7 +1,7 @@
 import { forwardRef } from "react"
 import { useControlledState } from "../../hooks/useControllableState"
 import { cx } from "jh-generated/cx"
-import { slider } from "jh-generated/recipes"
+import { slider, type SliderVariant } from "jh-generated/recipes"
 
 type SliderProps = {
   min?: number
@@ -11,26 +11,45 @@ type SliderProps = {
   value?: number
   setValue?: (value: number) => void
   defaultValue?: number
-}
+} & Partial<SliderVariant>
 
+const RANGE_WIDTH: string = "--range-size" //style prop 타입에러로 인해 string타입으로 사용
 export const Slider = forwardRef<HTMLInputElement, SliderProps>(
   (props, ref) => {
-    const { value, setValue, defaultValue, className, ...restProps } = props
-    const [slideValue, setSlideValue] = useControlledState({
+    const {
+      value,
+      setValue,
+      defaultValue,
+      className,
+      orientation,
+      max = 100,
+      min = 0,
+      ...restProps
+    } = props
+    const [slideValue = min, setSlideValue] = useControlledState({
       prop: value,
       onChange: setValue,
       defaultProp: defaultValue,
     })
 
     return (
-      <input
-        type="range"
-        className={cx(slider(), className)}
-        value={slideValue}
-        ref={ref}
-        {...restProps}
-        onChange={(e) => setSlideValue(Number(e.target.value))}
-      />
+      <>
+        <input
+          type="range"
+          tabIndex={0} //safari에서 focus 잡히지 않아서 추가
+          className={cx(slider({ orientation }), className)}
+          value={slideValue}
+          ref={ref}
+          max={max}
+          min={min}
+          {...restProps}
+          style={{
+            [RANGE_WIDTH]: `${((slideValue - min) / (max - min)) * 100}% 100%`,
+          }}
+          onChange={(e) => setSlideValue(Number(e.target.value))}
+        />
+      </>
     )
   },
 )
+//FIX : 사파리에서 키보드이벤트 실행 안됨
