2e8c081dd611d83164012ebb937225d46b4a2bb3
이종혁 <whdgur57117@gmail.com>
2024-08-31 00:06:07 +0900

    Avatar storybook 추가

--- packages/ui/src/stories/Avatar.stories.tsx
+++ packages/ui/src/stories/Avatar.stories.tsx
@@ -2,6 +2,9 @@ import type { Meta, StoryObj } from "@storybook/react"
 
 import { Avatar } from "../components/Avatar/Avatar.tsx"
 
+const TEST_SRC =
+  "https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80"
+
 const meta = {
   title: "Avatar",
   component: Avatar,
@@ -14,6 +17,12 @@ type Story = StoryObj<typeof meta>
 
 export const Primary: Story = {
   args: {
-    src: "https://images.unsplash.com/photo-1511485977113-f34c92461ad9?ixlib=rb-1.2.1&w=128&h=128&dpr=2&q=80",
+    src: TEST_SRC,
+  },
+}
+
+export const Controlled: Story = {
+  render: () => {
+    return <Avatar src="fake" onStatusChange={(status) => alert(status)} />
   },
 }
