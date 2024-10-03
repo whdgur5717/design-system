ebaf210e2bcba6380124ec58d45bd3550a66aef4
이종혁 <whdgur57117@gmail.com>
2024-10-03 20:43:37 +0900

    Slider 컴포넌트 리팩토링

--- packages/ui/src/components/Slider/Slider.tsx
+++ packages/ui/src/components/Slider/Slider.tsx
@@ -1,55 +1,140 @@
-import { forwardRef, type ComponentPropsWithoutRef } from "react"
+import { useState } from "react"
 import { useControlledState } from "../../hooks/useControllableState"
-import { cx } from "jh-generated/cx"
-import { slider, type SliderVariant } from "jh-generated/recipes"
 
-type SliderProps = {
-  min?: number
-  max?: number
-  className?: string
-  step?: number
-  value?: number
-  setValue?: (value: number) => void
+type Props = {
+  min: number
+  max: number
+  value: number
   defaultValue?: number
-  "aria-label": string
-} & Partial<SliderVariant> &
-  ComponentPropsWithoutRef<"input">
+  onChange: (value: number) => void
+}
 
-const RANGE_WIDTH: string = "--range-size" //style prop 타입에러로 인해 string타입으로 사용
-export const Slider = forwardRef<HTMLInputElement, SliderProps>(
-  (props, forwardRef) => {
-    const {
-      value,
-      setValue,
-      defaultValue,
-      className,
-      orientation,
-      max = 100,
-      min = 0,
-      ...restProps
-    } = props
-    const [slideValue = min, setSlideValue] = useControlledState({
-      prop: value,
-      onChange: setValue,
-      defaultProp: defaultValue,
-    })
+const getPercentage = (value: number, min: number, max: number) =>
+  ((value - min) / (max - min)) * 100
 
-    return (
-      <input
-        type="range"
-        tabIndex={0} //safari에서 focus 잡히지 않아서 추가
-        className={cx(slider({ orientation: "horizontal" }), className)}
-        value={slideValue}
-        ref={forwardRef}
-        max={max}
-        min={min}
-        {...restProps}
+const isTouchEvent = (e: TouchEvent | MouseEvent): e is TouchEvent => {
+  return e && "touches" in e
+}
+
+const isMouseEvent = (e: TouchEvent | MouseEvent): e is MouseEvent => {
+  return e && "screenX" in e
+}
+
+const getClientX = (
+  e: React.TouchEvent<HTMLElement> | React.MouseEvent<HTMLElement>,
+) => {
+  let clientX = 0
+  if (isMouseEvent(e.nativeEvent)) {
+    clientX = e.nativeEvent.clientX
+  } else if (isTouchEvent(e.nativeEvent)) {
+    clientX =
+      e.nativeEvent.touches.length > 0
+        ? e.nativeEvent.touches[0].clientX
+        : e.nativeEvent.changedTouches[0].clientX
+  }
+  return clientX
+}
+
+export const Slider = ({
+  min = 0,
+  max = 100,
+  value,
+  onChange,
+  defaultValue,
+}: Props) => {
+  const [isDragging, setIsDragging] = useState(false)
+
+  const [innerValue = 0, setValue] = useControlledState({
+    prop: value,
+    defaultProp: defaultValue || min,
+    onChange,
+  })
+
+  const handlePointerDown = (e: React.PointerEvent) => {
+    e.preventDefault()
+    setIsDragging(true)
+    e.currentTarget.setPointerCapture(e.pointerId)
+  }
+
+  const handlePointerUp = (e: React.PointerEvent) => {
+    e.preventDefault()
+    setIsDragging(false)
+    e.currentTarget.releasePointerCapture(e.pointerId)
+  }
+
+  const percentage = getPercentage(innerValue, min, max)
+
+  const handleMouseMove = (e: React.PointerEvent<HTMLElement>) => {
+    e.preventDefault()
+    if (!isDragging) return
+    //width는 본인의 상위태그여야함..
+    const { left, width } = e.currentTarget.getBoundingClientRect()
+    let percentage = (getClientX(e) - left) / width
+    console.log(left, width)
+    percentage = Math.min(Math.max(percentage, 0), 1)
+    console.log(percentage)
+    const newValue = min + percentage * (max - min)
+    setValue(newValue)
+  }
+
+  return (
+    <>
+      <span
+        className="slider-wrapper"
         style={{
-          [RANGE_WIDTH]: `${((slideValue - min) / (max - min)) * 100}% 100%`,
+          position: "relative",
+          display: "flex",
+          alignItems: "center",
+          width: "200px",
+          height: "50px",
         }}
-        onChange={(e) => setSlideValue(Number(e.target.value))}
-      />
-    )
-  },
-)
-//FIX : 사파리에서 키보드이벤트 실행 안됨
+      >
+        <span
+          className="slider-track"
+          onPointerDownCapture={handlePointerDown}
+          onPointerUpCapture={handlePointerUp}
+          onPointerMoveCapture={handleMouseMove}
+          onPointerCancelCapture={handlePointerUp}
+          style={{
+            position: "relative",
+            backgroundColor: "black",
+            flexGrow: 1,
+            borderRadius: "9999px",
+            height: "10px",
+          }}
+        >
+          <span
+            className="slider-value"
+            style={{
+              backgroundColor: "white",
+              borderRadius: "9999px",
+              height: "100%",
+              position: "absolute",
+              width: `${percentage}%`,
+            }}
+          ></span>
+          <span
+            className="slider-thumb"
+            style={{
+              position: "absolute",
+              left: `${percentage}%`,
+              transform: "translateX(-50%)",
+            }}
+          >
+            <span
+              style={{
+                width: "10px",
+                height: "10px",
+                borderRadius: "100px",
+                backgroundColor: "pink",
+                opacity: "0.5",
+                display: "block",
+              }}
+            ></span>
+          </span>
+        </span>
+      </span>
+      <div style={{ color: "pink" }}>{innerValue}</div>
+    </>
+  )
+}
